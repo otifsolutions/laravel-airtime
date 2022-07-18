48129b6f67a8d2a9bc9580d5cbf8fdfc1e5e5fd9
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-18 19:23:23 +0500

    added models

--- src/Models/ReloadlyPromotion.php
+++ src/Models/ReloadlyPromotion.php
@@ -4,8 +4,6 @@ namespace OTIFSolutions\LaravelAirtime\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ReloadlyOperator extends Model {
-
-    protected $guarded = [];
+class ReloadlyPromotion extends Model {
 
 }
