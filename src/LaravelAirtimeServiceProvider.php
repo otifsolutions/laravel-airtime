fa865a6ded358f6805af741dd676d9f1efc922e5
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-18 17:39:27 +0500

    made change

---
+++ src/LaravelAirtimeServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OTIFSolutions\LaravelAirtime\AirtimeServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAirtimeServiceProvider extends ServiceProvider {
+
+    public function register() {
+
+    }
+
+    public function boot() {
+
+    }
+}
