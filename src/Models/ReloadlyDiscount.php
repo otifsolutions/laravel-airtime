0ae38d234ee8bf63d9c38ced528976d17f450f55
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-19 13:53:09 +0500

    added softDelete trait to these models

--- src/Models/ReloadlyDiscount.php
+++ src/Models/ReloadlyDiscount.php
@@ -3,7 +3,10 @@
 namespace OTIFSolutions\LaravelAirtime\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReloadlyDiscount extends Model {
+    use SoftDeletes;
+    
     protected $guarded = [];
 }
--- src/Models/ReloadlyOperator.php
+++ src/Models/ReloadlyOperator.php
@@ -3,8 +3,10 @@
 namespace OTIFSolutions\LaravelAirtime\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReloadlyOperator extends Model {
+    use SoftDeletes;
 
     protected $guarded = [];
 
