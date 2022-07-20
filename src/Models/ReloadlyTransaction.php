4e72d5aba77ad7ba8b8de6d129f49cf134fa222f
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-20 17:21:16 +0500

    minor change

--- src/Models/ReloadlyTransaction.php
+++ src/Models/ReloadlyTransaction.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class ReloadlyTransaction extends Model {
     protected $guarded = [];
 
+    protected $casts = [
+        'response' => 'json',
+        'pin' => 'json'
+    ];
+
     public function operator() {
         return $this->belongsTo(ReloadlyOperator::class);
     }
