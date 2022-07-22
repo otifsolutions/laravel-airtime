58e1f2ca0e7e2f26482896f823ece1bae68b8cf4
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-22 15:10:22 +0500

    added missing colomn engine for table

--- src/Database/migrations/2022_07_21_133231_create_value_topup_operators_table.php
+++ src/Database/migrations/2022_07_21_133231_create_value_topup_operators_table.php
@@ -4,16 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration {
+
+    public function up() {
         Schema::create('value_topup_operators', function (Blueprint $table) {
+            $table->engine = 'myIsam';
             $table->id();
 
             $table->foreignId('country_id')
@@ -30,13 +25,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('value_topup_operators');
     }
 };
