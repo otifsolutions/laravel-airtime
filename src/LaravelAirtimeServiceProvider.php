8beeb55372e73465d72184dc3d0d81786052141a
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-26 16:24:31 +0500

    value topup commands added in service provider

--- src/LaravelAirtimeServiceProvider.php
+++ src/LaravelAirtimeServiceProvider.php
@@ -4,6 +4,8 @@ namespace OTIFSolutions\LaravelAirtime\AirtimeServiceProvider;
 
 use Illuminate\Support\ServiceProvider;
 use OTIFSolutions\LaravelAirtime\Commands\SyncReloadly;
+use OTIFSolutions\LaravelAirtime\Commands\SyncValueTopup;
+use OTIFSolutions\LaravelAirtime\Commands\SyncValueTopupStatus;
 
 class LaravelAirtimeServiceProvider extends ServiceProvider {
 
@@ -16,6 +18,8 @@ class LaravelAirtimeServiceProvider extends ServiceProvider {
 
         if ($this->app->runningInConsole()) {
             $this->commands([
+                SyncValueTopup::class,
+                SyncValueTopupStatus::class,
                 SyncReloadly::class
             ]);
         }
