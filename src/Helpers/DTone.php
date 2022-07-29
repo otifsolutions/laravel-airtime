9c14666d4bfd2241d1678a0444b6ebeb2991a40e
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-29 14:50:07 +0500

    helper class for DTone added

---
+++ src/Helpers/DTone.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace OTIFSolutions\LaravelAirtime\Helpers;
+
+use OTIFSolutions\CurlHandler\Curl;
+use OTIFSolutions\Laravel\Settings\Models\Setting;
+use OTIFSolutions\LaravelAirtime\Models\DToneCountry;
+use OTIFSolutions\LaravelAirtime\Models\DToneOperator;
+use OTIFSolutions\LaravelAirtime\Models\DToneTransaction;
+
+class DTone {
+
+    public static function TShop($username = null, $token = null) {
+        return new class($username, $token) {
+
+            protected $username;
+            protected $token;
+            private $url = "https://airtime-api.dtone.com/cgi-bin/shop/topup";
+
+            private function makeRequest($body) {
+                $rand = random_int(0, time() / 2);
+                $time = time() - $rand;
+                $xml = "<xml>" .
+                    "<login>" . $this->username . "</login>" .
+                    "<key>" . $time . "</key>" .
+                    "<md5>" . md5($this->username . $this->token . $time) . "</md5>" .
+                    $body .
+                    "</xml>";
+                $response = Curl::Make()->POST->url($this->url)->header([
+                    'Content-Type: text/xml;'
+                ])->body($xml)->execute();
+                if ($response['TransferTo']['error_code'] === "925") {
+                    if (Setting::get('mattermost_bot_token')) {
+                        Curl::Make()->POST->url('https://team.otif-solutions.com/api/v4/posts')
+                            ->header([
+                                'Authorization: Bearer ' . Setting::get('mattermost_bot_token')
+                            ])->body([
+                                'channel_id' => "k7475c6g1b8i9k995wxbjjxgsa",
+                                'message' => "DTONE Balance Response : ```" . json_encode($response, JSON_THROW_ON_ERROR) . "```"
+                            ])->execute();
+                    }
+                    return $this->makeRequest($body);
+                }
+                return $response;
+            }
+
+            public function __construct($username, $token) {
+                $this->username = $username;
+                $this->token = $token;
+            }
+
+            public function setCredentials($username, $token): object {
+                $this->username = $username;
+                $this->token = $token;
+
+                return $this;
+            }
+
+            public function getBalance(): ?string {
+                $response = $this->makeRequest("<action>check_wallet</action>");
+                return $response["TransferTo"]["balance"] ?? null;
+            }
+
+            public function getAccountInformation($number) {
+                $response = $this->makeRequest("<action>msisdn_info</action><destination_msisdn>" . $number . "</destination_msisdn>");
+                if (isset($response['TransferTo']['operatorid'])) {
+                    return DToneOperator::where('t_shop_id', $response['TransferTo']['operatorid'])->first();
+                }
+                return null;
+            }
+
+            public function getOperators(DToneCountry $country) {
+                $response = $this->makeRequest("<info_type>country</info_type><content>" . $country['t_shop_id'] . "</content><action>pricelist</action>");
+                return $response['TransferTo'] ?? null;
+            }
+
+            public function getProducts($operatorId) {
+                $response = $this->makeRequest("<info_type>operator</info_type><content>" . $operatorId . "</content><action>pricelist</action>");
+                return $response['TransferTo'] ?? null;
+            }
+
+            /**
+             * @param DToneTransaction $transaction
+             * @return bool
+             */
+            public function sendTransfer(DToneTransaction $transaction): bool {
+                $number = $transaction['number'];
+                if (strpos($number, "0") === 0) {
+                    $number = substr($number, 1);
+                }
+                $code = $transaction['operator']['country']['dial_code'];
+                if (!(strpos($number, $code) === 0 || strpos($number, "+" . $code) === 0)) {
+                    $number = $code . $number;
+                }
+                if (str_contains(strtoupper($transaction['operator']['name']), 'PIN'))
+                    $transaction['response'] = $this->makeRequest("<msisdn>" . $transaction['sender_phone_no'] . "</msisdn><destination_msisdn>" . $number . "</destination_msisdn><operatorid>" . $transaction['operator']['t_shop_id'] . "</operatorid> <product>" . $transaction['product'] . "</product><action>topup</action>");
+                else
+                    $transaction['response'] = $this->makeRequest("<msisdn>" . $transaction['sender_phone_no'] . "</msisdn><destination_msisdn>" . $number . "</destination_msisdn><product>" . $transaction['product'] . "</product><action>topup</action>");
+                if ($transaction['response']["TransferTo"]["error_code"] === "0") {
+                    $transaction['status'] = 'SUCCESS';
+                    $transaction->save();
+                    return true;
+                }
+                $transaction['status'] = 'FAIL';
+                $transaction->save();
+                return false;
+            }
+
+        };
+    }
+
+}
