55b7272bd140acb2be93be063f8958e73860a7cd
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-08-02 14:39:01 +0500

    removed seperate command for syncing countires json, and moved the code into the Basic/parent command SyncDingConnect, just after the credentials found checks

--- src/Commands/SyncDingConnect.php
+++ src/Commands/SyncDingConnect.php
@@ -34,6 +34,38 @@ class SyncDingConnect extends Command {
             return 0;
         }
 
+        // --------- sycning countries form json------------------------------
+
+        $countries = json_decode(
+            file_get_contents(__DIR__ . '../../Database/jsons/countriesDingConnect.json'),
+            true, 512, JSON_THROW_ON_ERROR
+        );
+
+        $countCoutnries = count($countries);
+
+        $progressBar = $this->output->createProgressBar($countCoutnries);
+
+        $this->newLine();
+        $this->line('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+        $this->info('>>>>>>>>> Syncing countries table with json <<<<<<<<<<<<<');
+
+        $progressBar->start();
+        foreach ($countries as $country) {
+            DingConnectCountry::updateOrCreate(['iso2' => $country['iso2']], [
+                'name' => $country['name'],
+                'dial_code' => $country['dialCode']
+            ]);
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+        $this->newLine();
+        $this->info('>>>>>>>>>>>> ' . $countCoutnries . ' countries synced <<<<<<<<<<');
+        $this->line('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+        $this->newLine();
+
+        //  ----------------- syncing countries for ding-connect service ends
+
         $this->info("Credentials Found.");
 
         $this->line("Syncing Balance");
