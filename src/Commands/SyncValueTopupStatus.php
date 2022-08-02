1bdcdd3399ac012b2625ddfae514e105915cf46f
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-08-02 13:07:55 +0500

    tiny changes

--- src/Commands/SyncValueTopupStatus.php
+++ src/Commands/SyncValueTopupStatus.php
@@ -2,31 +2,16 @@
 
 namespace OTIFSolutions\LaravelAirtime\Commands;
 
+use Illuminate\Console\Command;
 use OTIFSolutions\LaravelAirtime\Helpers\ValueTopup;
 use OTIFSolutions\LaravelAirtime\Models\ValueTopupTransaction;
-use Illuminate\Console\Command;
-use OTIFSolutions\Laravel\Settings\Models\Setting;
 
 class SyncValueTopupStatus extends Command {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'sync:valuetopupstatus';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Sync status of transactions that are still in processing';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle() {
         $this->line("");
         $this->line("****************************************************************");
@@ -39,15 +24,14 @@ class SyncValueTopupStatus extends Command {
         $this->line("****************************************************************");
         $this->line("Fetching transactions from database with status processing");
 
-
         $transactions = ValueTopupTransaction::where("status", "PROCESSING")->get();
-        if(count($transactions) === 0){
+        if (count($transactions) === 0) {
             $this->info("No Transactions with status Processing Found");
         }
 
-        foreach ($transactions as $transaction){
+        foreach ($transactions as $transaction) {
             $response = ValueTopup::Make()->getValueTopupStatus($transaction['reference']);
-            if($response['responseCode'] === '000')
+            if ($response['responseCode'] === '000')
                 $transaction['status'] = 'SUCCESS';
             elseif ($response['responseCode'] == '851' || $response['responseCode'] == '852')
                 $transaction['status'] = 'PROCESSING';
