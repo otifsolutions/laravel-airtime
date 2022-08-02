a697c3bd5c3fcbd76aff853cf45d7526d2f7f3ca
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-08-02 14:54:46 +0500

    moved code from syncing countries json to this command

--- src/Commands/SyncDTone.php
+++ src/Commands/SyncDTone.php
@@ -33,6 +33,43 @@ class SyncDTone extends Command {
         }
         $this->info("Credentials Found.");
 
+        // ------------ syncing countries from json for service d-tone ----------------------
+        $countries = json_decode(
+            file_get_contents(__DIR__ . '../../Database/jsons/countriesDingConnect.json'),
+            true, 512, JSON_THROW_ON_ERROR
+        );
+
+        $countCoutnries = count($countries);
+
+        $progressBar = $this->output->createProgressBar($countCoutnries);
+
+        $this->newLine();
+        $this->line('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+        $this->info('>>>>>>>>> Syncing countries table with json <<<<<<<<<<<<<');
+        // $this->line('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+
+        $progressBar->start();
+        foreach ($countries as $country) {
+            if (isset($country['tshop_id'])) {
+                DToneCountry::updateOrCreate(['iso2' => $country['iso2']], [
+                    'name' => $country['name'],
+                    'dial_code' => $country['dialCode'],
+                    't_shop_id' => $country['tshop_id']
+                ]);
+            }
+
+            $progressBar->advance();
+
+        }
+
+        $progressBar->finish();
+
+        $this->newLine();
+        $this->info('>>>>>>>>>>>> ' . $countCoutnries . ' countries synced <<<<<<<<<<');
+        $this->line('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+        $this->newLine();
+        // ------------- syncing d-tone service countries from json ends here ----------------
+
         $this->line("Syncing Balance");
         $balance = DTone::TShop($credentials['name'], $credentials['token'])->getBalance();
         Setting::set('d_tone_balance', $balance, 'STRING');
