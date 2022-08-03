47b835eb5d200372797e011eddfdfac32d4f7a9e
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-08-03 16:29:33 +0500

    run only a single migration airtime_currencies when user does php artisan migrate

--- src/AirtimeServiceProvider.php
+++ src/AirtimeServiceProvider.php
@@ -17,7 +17,8 @@ class AirtimeServiceProvider extends ServiceProvider {
     }
 
     public function boot() {
-        // $this->loadMigrationsFrom(__DIR__ . '/Database/migrations/');
+        // $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/Database/migrations/2022_07_18_130511_create_airtime_currencies_table.php');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
